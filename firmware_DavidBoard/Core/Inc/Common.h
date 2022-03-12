48ffe17361024eb10983b5a74a2a88c7c6184d4e
Anthony Berbari <fberbari@edu.uwaterloo.ca>
2022-03-12 00:18:35 -0500

    I2C driver for ADC works

--- firmware_DavidBoard/Core/Inc/Common.h
+++ firmware_DavidBoard/Core/Inc/Common.h
@@ -3,6 +3,7 @@
 #include "main.h"
 
 #include <stdbool.h>
+#include <stdint.h>
 
 /**
  * This module holds any definitions, structures, functions or otherwise
@@ -10,7 +11,7 @@
  */
 
 /***********************************************************************************************************************
- * Definitions
+ * General Definitions
  **********************************************************************************************************************/
 typedef struct
 {
@@ -27,3 +28,20 @@ typedef struct
 #define RETROFRIGERATION_BUSY         2
 #define RETROFRIGERATION_SUCCEEDED    1
 #define RETROFRIGERATION_FAILED       0
+
+/***********************************************************************************************************************
+ * State data structures definitions
+ **********************************************************************************************************************/
+
+#define FAN_OFF 0
+#define FAN_ON 1
+
+#define COMPRESSOR_OFF 0
+#define COMPRESSOR_ON 1
+
+typedef struct
+{
+    int fan;          // either FAN_ON or FAN_OFF (all other values illegal)
+    int compressor;   // either COMPRESSOR_ON or COMPRESSOR_OFF (all other values illegal)
+
+}ActuatorCommands_t;
