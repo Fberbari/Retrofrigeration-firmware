215da58dda94a74fa7872edccb92742a1c90d49a
Anthony Berbari <fberbari@edu.uwaterloo.ca>
2022-03-12 17:53:27 -0500

    continued work on I2C manager

--- firmware/Core/Src/I2CManager.c
+++ firmware/Core/Src/I2CManager.c
@@ -5,74 +5,115 @@
  * Definitions
  **********************************************************************************************************************/
 
-typedef struct
+#define ADC_SLAVE_ADDRESS 0x33
+#define ADC_SLAVE_ADDRESS_W (ADC_SLAVE_ADDRESS << 1)
+#define ADC_SLAVE_ADDRESS_R (ADC_SLAVE_ADDRESS << 1) | 0x01
+
+// [7:7] Setup byte select, [6:4] Use internal reference, [3:3] external clock,
+// [2:2] uni-polar mode, [1:1] don't reset config register, [0:0] don't care.
+#define ADC_SETUP_BYTE 0xDA // 11011010
+
+// [7:7] Configuration byte select, [6:5] scan select, [4:1] channel select, [0:0] single ended mode
+#define ADC_CONFIG_BYTE 0x0F //00001111
+
+// Only used for the initialization polling I2C functions
+#define DEFAULT_TIMEOUT 1000 // ms
+
+typedef enum I2CbusStatus
 {
-    uint16_t thermistor[4];  // 10 bit raw ADC values corresponding to each thermistor
+    READY,
+    BUSY_IOEXPAND_1,
+    BUSY_IOEXPAND_2,
+    BUSY_ADC,
+    FAILED
+}I2CBusStatus_t;
 
-}ThermistorADC_t;
+/***********************************************************************************************************************
+ * Variables
+ **********************************************************************************************************************/
 
-#define BUTTON_OPEN 0
-#define BUTTON_PRESSED 1
+// HAL handle
+extern I2C_HandleTypeDef hi2c2;
 
-typedef struct
-{
-    int button[4];  // each element corresponds to a button state. Legal states are BUTTON_OPEN and BUTTON_PRESSED
 
-}PushButtonStates_t;
+static uint8_t rawAdcData[16];
 
+
+static I2CBusStatus_t currentBusStatus;
 
 /***********************************************************************************************************************
  * Prototypes
  **********************************************************************************************************************/
 
-/**
-* Initializes internal parameters.
-* Should be called exactly once before anything is attempted to be done with this module.
-* Failing to call this function, or calling it more than once, voids all guarantees made by this module.
-*/
-void I2CManager_Init(void);
+static void InitAdc(void);
+static void LaunchADCExchange(void);
 
-/**
-* Provides the most recently collected data from the thermistor ADC.
-* Returns succeeded if the data is valid, failed if it is invalid and busy if the reading has not been
-* updated since last time this function was called.
-* This is a non blocking function that returns right away.
-* @param[out]        ThermistorADC          struct containing the most recent ADC readings
-* @return           RETROFRIGERATION_SUCCEEDED, RETROFRIGERATION_BUSY or RETROFRIGERATION_FAILED
-*/
-int I2CManager_GetRawThermistorADC(ThermistorADC_t *ThermistorADC);
+/***********************************************************************************************************************
+ * Code
+ **********************************************************************************************************************/
 
-/**
-* Provides the most recently measured states of all buttons in the system.
-* Returns succeeded if the data is valid, failed if it is invalid and busy if the reading has not been
-* updated since last time this function was called.
-* This is a non blocking function that returns right away.
-* @param[out]        PushButtonStates          struct containing the most recent button states
-* @return           RETROFRIGERATION_SUCCEEDED, RETROFRIGERATION_BUSY or RETROFRIGERATION_FAILED
-*/
-int I2CManager_GetPushButtonStates(PushButtonStates_t *PushButtonStates);
+void I2CManager_Init(void)
+{
+    currentBusStatus = READY;
 
-/**
-* Sets the appropriate bits in the I/O expander to the states specified in the input struct.
-* Returns succeeded if the PREVIOUS transmission succeeded, failed if the previous transmission failed,
-* and busy if the previous command was not yet transmitted. In the case of a busy return, the previous values
-* will be overwritten and the module will attempt to send the most recent commands.
-* This is a non blocking function that returns right away.
-* @param[in]        ActuatorCommands         the desired actuator states
-* @return           RETROFRIGERATION_SUCCEEDED, RETROFRIGERATION_BUSY or RETROFRIGERATION_FAILED
-*/
-int I2CManager_SendActuatorCommands(const ActuatorCommands_t *ActuatorCommands);
+    InitAdc();
+}
 
+int I2CManager_GetRawThermistorADC(ThermistorADC_t *ThermistorADC)
+{
 
-// TOO Jacob to add LCD function
+}
 
+int I2CManager_GetPushButtonStates(PushButtonStates_t *PushButtonStates)
+{
 
-/**
-* Begins all I2C exchanges necessary to collect and transmit all the above data.
-* This function should be called exactly once per control loop period, though is safe to call more often.
-* Returns succeeded if the transmission was started successfully, failed if the transmission failed to start
-* and busy if an exchange is in progress. In the case of a busy return, nothing happens.
-* This is a non blocking function that returns right away.
-* @return           RETROFRIGERATION_SUCCEEDED, RETROFRIGERATION_BUSY or RETROFRIGERATION_FAILED
-*/
-int I2CManager_LaunchExchange(void);
+}
+
+int I2CManager_SendActuatorCommands(const ActuatorCommands_t *ActuatorCommands)
+{
+
+}
+
+int I2CManager_LaunchExchange(void)
+{
+
+}
+
+static void InitAdc(void)
+{
+
+    uint8_t data[2] = {ADC_SETUP_BYTE, ADC_CONFIG_BYTE};
+
+    currentBusStatus = BUSY_ADC;
+
+    HAL_I2C_Master_Transmit(&hi2c2, ADC_SLAVE_ADDRESS_W, data, sizeof(data), DEFAULT_TIMEOUT);
+
+    currentBusStatus = READY;
+}
+
+static void LaunchADCExchange(void)
+{
+    currentBusStatus = BUSY_ADC;
+    HAL_I2C_Master_Receive_IT(&hi2c2, ADC_SLAVE_ADDRESS_R, rawAdcData, sizeof(rawAdcData));
+}
+
+/****************************Interrupt Handlers****************************/
+
+void HAL_I2C_MasterRxCpltCallback(I2C_HandleTypeDef *hi2c)
+{
+    switch(currentBusStatus)
+    {
+        case BUSY_IOEXPAND_1:
+            break;
+
+        case BUSY_IOEXPAND_2:
+            break;
+
+        case BUSY_ADC:
+            break;
+
+        default:
+            currentBusStatus = FAILED;
+            break;
+    }
+}
--- firmware/Core/Src/main.c
+++ firmware/Core/Src/main.c
@@ -41,7 +41,9 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
- TIM_HandleTypeDef htim2;
+ I2C_HandleTypeDef hi2c2;
+
+TIM_HandleTypeDef htim2;
 
 /* USER CODE BEGIN PV */
 
@@ -51,6 +53,7 @@
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_TIM2_Init(void);
+static void MX_I2C2_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -89,9 +92,15 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_TIM2_Init();
+  MX_I2C2_Init();
   /* USER CODE BEGIN 2 */
 
+  // Interrupts are disabled for the setup portion.
+  __disable_irq();
+
   Controller_Init();
+
+  __enable_irq();
 
   /* USER CODE END 2 */
 
@@ -150,6 +159,54 @@ void SystemClock_Config(void)
 }
 
 /**
+  * @brief I2C2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C2_Init(void)
+{
+
+  /* USER CODE BEGIN I2C2_Init 0 */
+
+  /* USER CODE END I2C2_Init 0 */
+
+  /* USER CODE BEGIN I2C2_Init 1 */
+
+  /* USER CODE END I2C2_Init 1 */
+  hi2c2.Instance = I2C2;
+  hi2c2.Init.Timing = 0x00303D5B;
+  hi2c2.Init.OwnAddress1 = 0;
+  hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c2.Init.OwnAddress2 = 0;
+  hi2c2.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c2, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c2, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C2_Init 2 */
+
+  /* USER CODE END I2C2_Init 2 */
+
+}
+
+/**
   * @brief TIM2 Initialization Function
   * @param None
   * @retval None
@@ -205,6 +262,7 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
