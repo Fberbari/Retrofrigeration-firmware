c40221376a8f68d1586ab468fc037148e9e59ba1
Anthony Berbari <fberbari@edu.uwaterloo.ca>
2022-03-12 21:06:31 -0500

    I2C driver for ADC works

--- firmware_DavidBoard/Core/Src/I2CManager.c
+++ firmware_DavidBoard/Core/Src/I2CManager.c
@@ -37,6 +37,7 @@ extern I2C_HandleTypeDef hi2c2;
 
 
 static uint8_t rawAdcData[16];
+static bool adcDataNew;
 
 
 static I2CBusStatus_t currentBusStatus;
@@ -56,27 +57,50 @@ void I2CManager_Init(void)
 {
     currentBusStatus = READY;
 
+    adcDataNew = false;
+
     InitAdc();
 }
 
 int I2CManager_GetRawThermistorADC(ThermistorADC_t *ThermistorADC)
 {
+    if(currentBusStatus == FAILED)
+    {
+        return RETROFRIGERATION_FAILED;
+    }
+
+    if (! adcDataNew)
+    {
+        return RETROFRIGERATION_BUSY;
+    }
+
+
+    ThermistorADC->thermistor[0] = rawAdcData[8] + ((rawAdcData[9] & 0x3) << 8);
+    ThermistorADC->thermistor[1] = rawAdcData[10] + ((rawAdcData[11] & 0x3) << 8);
+    ThermistorADC->thermistor[2] = rawAdcData[12] + ((rawAdcData[13] & 0x3) << 8);
+    ThermistorADC->thermistor[3] = rawAdcData[14] + ((rawAdcData[15] & 0x3) << 8);
+
+    adcDataNew = false;
+
+    return RETROFRIGERATION_SUCCEEDED;
 
 }
 
 int I2CManager_GetPushButtonStates(PushButtonStates_t *PushButtonStates)
 {
-
+    return RETROFRIGERATION_SUCCEEDED;
 }
 
 int I2CManager_SendActuatorCommands(const ActuatorCommands_t *ActuatorCommands)
 {
-
+    return RETROFRIGERATION_SUCCEEDED;
 }
 
 int I2CManager_LaunchExchange(void)
 {
-
+    // temporary Once the other 2 drivers work, what will happen is we start by triggering the LCD, then once that completes we trigger the second IO expander, then the ADC
+	currentBusStatus = BUSY_ADC;
+    LaunchADCExchange();
 }
 
 static void InitAdc(void)
@@ -93,7 +117,6 @@ static void InitAdc(void)
 
 static void LaunchADCExchange(void)
 {
-    currentBusStatus = BUSY_ADC;
     HAL_I2C_Master_Receive_IT(&hi2c2, ADC_SLAVE_ADDRESS_R, rawAdcData, sizeof(rawAdcData));
 }
 
@@ -104,12 +127,18 @@ void HAL_I2C_MasterRxCpltCallback(I2C_HandleTypeDef *hi2c)
     switch(currentBusStatus)
     {
         case BUSY_IOEXPAND_1:
+            // launch I/0 expander 2
+            currentBusStatus = BUSY_IOEXPAND_2;
             break;
 
         case BUSY_IOEXPAND_2:
+            // launch ADC
+            currentBusStatus = BUSY_ADC;
             break;
 
         case BUSY_ADC:
+            currentBusStatus = READY;
+            adcDataNew = true;
             break;
 
         default:
